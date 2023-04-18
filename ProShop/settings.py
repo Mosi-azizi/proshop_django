200ed4bff109c64b59536bd538a91d60f840568c
SAM <85979367+Mosi-azizi@users.noreply.github.com>
2023-04-18 16:24:07 +0330

    use django reset_framework

--- ProShop/settings.py
+++ ProShop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products.apps.ProductsConfig'
+    'rest_framework',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
